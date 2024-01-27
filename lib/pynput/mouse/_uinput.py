6ccb70728ce42d09d057aba0f97f2c3301377d37
Athul Raj Kollareth <krathul3152@gmail.com>
2024-01-28 04:46:34 +0530

    Add uinput backend for mouse

---
+++ lib/pynput/mouse/_uinput.py
@@ -0,0 +1,147 @@
+import enum
+import errno
+import functools
+import os
+import re
+import subprocess
+
+from evdev import (
+    InputDevice,
+    InputEvent,
+    UInput,
+    ecodes
+)
+
+from pynput._util.uinput import ListenerMixin
+from . import _base
+
+Button = enum.Enum(
+    'Button',
+    module=__name__,
+    names=[
+        ('unknown', None),
+        ('left', ecodes.BTN_LEFT),
+        ('middle', ecodes.BTN_MIDDLE),
+        ('right', ecodes.BTN_RIGHT),
+    ]
+)
+
+class Controller(_base.Controller):
+    """
+    Takes an argument position getter (couldn't come up with a name), which is the function
+    that returns the position of the mouse cursor.
+    A function that queries the compositor for cursor position
+    """
+    def __init__(self, _position_getter = None, *args, **kwargs):
+        super(Controller, self).__init__(*args, **kwargs)
+        self._position_getter = _position_getter
+        if(_position_getter == None):
+            print("position getter is not assigned, position of cursor can't be queried")
+        #position of mouse is a 32-bit signed integer, hence the minimum position
+        self.INT_MIN32 = -2**31
+        self._capabilities = {
+            ecodes.EV_KEY : [ecodes.BTN_LEFT, ecodes.BTN_RIGHT, ecodes.BTN_MIDDLE],
+            ecodes.EV_REL : [
+                             ecodes.REL_X, 
+                             ecodes.REL_Y,
+                             ecodes.REL_WHEEL_HI_RES, 
+                             ecodes.REL_HWHEEL_HI_RES
+                            ]
+        }
+        self._dev = UInput(self._capabilities, name='Pynput-Mouse')
+
+    def __del__(self):
+        if hasattr(self, '_dev'):
+            self._dev.close()
+
+    def _position_get(self):
+        """
+        libevdev cannot be used to get the position of cursor, 
+        On cursor position is known only to the compositor.
+        While using uinput backend, assign _position_getter 
+        """
+        if self._position_getter == None:
+            raise NotImplementedError("Position getter not assigned")
+        
+        return self._position_getter
+
+    def _position_set(self, pos):
+        px, py = self._check_bounds(*pos)
+        self._dev.write(ecodes.EV_REL, ecodes.REL_X, self.INT_MIN32)
+        self._dev.write(ecodes.EV_REL, ecodes.REL_Y, self.INT_MIN32)
+        self._dev.write(ecodes.EV_REL, ecodes.REL_X, px)
+        self._dev.write(ecodes.EV_REL, ecodes.REL_Y, py)
+        self._dev.syn()
+
+    def move(self, dx, dy):
+        self._dev.write(ecodes.EV_REL, ecodes.REL_X, dx)
+        self._dev.write(ecodes.EV_REL, ecodes.REL_Y, dy)
+        self._dev.syn()
+
+    def _scroll(self, dx, dy):
+        dx, dy = self._check_bounds(dx, dy)
+        if dy:
+            self._dev.write(ecodes.EV_REL, ecodes.REL_WHEEL_HI_RES, dy)
+        if dx:
+            self._dev.write(ecodes.EV_REL, ecodes.REL_HWHEEL_HI_RES, dx)
+        self._dev.syn()
+
+    def _press(self, button : Button):
+        self._dev.write(ecodes.EV_REL, button.value, 1)
+        self._dev.syn()
+
+    def _release(self, button):
+        self._dev.write(ecodes.EV_REL, button.value, 0)
+        self._dev.syn()
+
+    def _check_bounds(self, *args):
+        """Checks the arguments and makes sure they are within the bounds of a
+        short integer.
+
+        :param args: The values to verify.
+        """
+        if not all(
+                (-0x7fff - 1) <= number <= 0x7fff
+                for number in args):
+            raise ValueError(args)
+        else:
+            return tuple(int(p) for p in args)
+
+
+class Listener(ListenerMixin, _base.Listener):
+    _EVENTS = (
+        ecodes.EV_KEY,
+        ecodes.EV_REL)
+
+    def __init__(self, *args, **kwargs):
+        super(Listener, self).__init__(*args, **kwargs)
+
+    def _handle(self, event : InputEvent):
+        if event.type == ecodes.EV_KEY:
+            self.on_click(self._button(event.code), event.value)
+
+        elif event.type == ecodes.EV_REL:
+            if(event.code == ecodes.REL_X):
+                self.on_move(event.value, 0)
+            elif(event.code == ecodes.REL_Y):
+                self.on_move(0, event.value)
+            elif(event.code == ecodes.REL_WHEEL_HI_RES):
+                self.on_scroll(0, event.value)
+            elif(event.code == ecodes.REL_HWHEEL_HI_RES):
+                self.on_scroll(event.value, 0)
+
+    # pylint: disable=R0201
+    def _button(self, code):
+        """Creates a mouse button from an event detail.
+
+        If the button is unknown, :attr:`Button.unknown` is returned.
+
+        :param detail: The event detail.
+
+        :return: a button
+        """
+        try:
+            return Button(code)
+        except ValueError:
+            return Button.unknown
+    # pylint: enable=R0201
--- lib/pynput/mouse/_xorg.py
+++ lib/pynput/mouse/_xorg.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-The keyboard implementation for *Xorg*.
+The mouse implementation for *Xorg*.
 """
 
 # pylint: disable=C0111
